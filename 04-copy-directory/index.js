cdccb9387030800e2d3aa756b478e6234801324a
Владимир Белый <belyy.vl@office.lan>
2023-05-04 11:03:37 +0300

    feat: add resolve for 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
 
+fs.mkdir(__dirname + '/files-copy', { recursive: true }, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+})
+
+fs.readdir(__dirname + '/files', (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    files.forEach((items) => {
+      fs.readFile(`${__dirname}/files/${items}`, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        fs.writeFile(`${__dirname}/files-copy/${items}`, data, (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        })
+
+      })
+    })
+})
